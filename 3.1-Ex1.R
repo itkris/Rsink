38b1d84f2de8912cd75014ae4ba697fbfdfaa54e
Krishnan Pazhayanoor <krisnan@gmail.com>
2016-03-21 10:09:41 -0400

    Got the cleanup script to work...

--- 3.1-Ex1.R
+++ 3.1-Ex1.R
@@ -6,12 +6,13 @@ library(tidyr)
 library(stringdist)
 
 masProdCat = data.frame( product_code = c("p", "v", "x", "q"), product_category = c('Smartphone', 'TV', 'Laptop', 'Tablet'), truth = c(1,1,1,1), stringsAsFactors = F)
-masCompany = data.frame( company = c('philips', 'akzo', 'van_houten', 'unilever'), stringsAsFactors=F)
+masCompany = data.frame( company = c('philips', 'akzo', 'van_houten', 'unilever'), truth = c(1,1,1,1), stringsAsFactors=F)
 
 greplace <- function(keyWords, searchVector) {
-  output <- data.frame(company = character(), match = logical(), value = character())
+  output <- data.frame(company = character(), match = logical(), Company = character())
+  searchVector <- unique(searchVector)
   for(stri in keyWords) {
-    output <- rbind(output, data.frame(company = searchVector, match = agrepl(stri, searchVector, ignore.case=T)) %>% filter(match==T) %>%  mutate(value=stri))
+    output <- rbind(output, data.frame(company = searchVector, match = agrepl(stri, searchVector, ignore.case=T)) %>% filter(match==T) %>%  mutate(Company=stri) )
   }
   return(output);
 }
@@ -25,17 +26,20 @@ wreplace <- function (keywords, searchString) {
   return (searchString)
 }
 
+
 # 0. load the data into R
 df_data <- tbl_df(read.xlsx("data/refine.xlsx", sheetIndex=1))
 
 # 1. clean up brand names
-df_data <- mutate(df_data, cleanCompany = wreplace(masCompany$company, company))
+df_cleancomps = unique(greplace(masCompany$company, df_data$company))
+df_data <- left_join(df_data, df_cleancomps, by="company") %>% select(-match, -company) # we dont need the match column 
+df_data$Company[is.na(df_data$Company)] <- "philips" #cleaning that one last outlier
 
 # 2. separate product code and number
 df_data <- separate(df_data, Product.code...number., into=c("product_code", "product_number"), sep='-')
 
 # 3. add product categories
-df_data <- inner_join(df_data, masProdCat)
+df_data <- inner_join(df_data, masProdCat, by="product_code")
 
 # 4. add full address for geocoding
 df_data <- df_data %>% unite(full_address, address, city, country, sep=", ")
@@ -45,4 +49,4 @@ df_data <- df_data %>% mutate(product_category = paste("product_", tolower(produ
 
 # 6. write to CSV file
 #View(df_data)
-# write.table(df_data, file="data/refine_clean.csv",sep=',', append=F, row.names=F)
+write.table(df_data, file="data/refine_clean.csv",sep=',', append=F, row.names=F)
