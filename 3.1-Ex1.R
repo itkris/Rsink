074ca0133781f4d1b864fe9a7485342c6cbf717f
Krishnan Pazhayanoor <krisnan@gmail.com>
2016-03-21 09:16:14 -0400

    trying wreplace in addition to greplace

--- 3.1-Ex1.R
+++ 3.1-Ex1.R
@@ -8,20 +8,28 @@ library(stringdist)
 masProdCat = data.frame( product_code = c("p", "v", "x", "q"), product_category = c('Smartphone', 'TV', 'Laptop', 'Tablet'), truth = c(1,1,1,1), stringsAsFactors = F)
 masCompany = data.frame( company = c('philips', 'akzo', 'van_houten', 'unilever'), stringsAsFactors=F)
 
-greplace <- function(searchStr, searchVector) {
-  #print(data.frame(searchVector))
-  print(masCompany[0])
-  output <- data.frame(oldStr = searchVector, match = agrepl(unique(searchStr), searchVector, ignore.case=T)) %>% mutate(value=ifelse(match, searchStr, ""))
+greplace <- function(keyWords, searchVector) {
+  output <- data.frame(company = character(), match = logical(), value = character())
+  for(stri in keyWords) {
+    output <- rbind(output, data.frame(company = searchVector, match = agrepl(stri, searchVector, ignore.case=T)) %>% filter(match==T) %>%  mutate(value=stri))
+  }
   return(output);
 }
 
-print(masCompany)
+wreplace <- function (keywords, searchString) {
+  for(keywrd in keywords) {
+      if(agrepl(keywrd, searchString, ignore.case=T)) {
+        return (keywrd)
+      }
+  }
+  return (searchString)
+}
 
 # 0. load the data into R
-df_data <-tbl_df(read.xlsx("data/refine.xlsx", sheetIndex=1))
+df_data <- tbl_df(read.xlsx("data/refine.xlsx", sheetIndex=1))
 
 # 1. clean up brand names
-
+df_data <- mutate(df_data, cleanCompany = wreplace(masCompany$company, company))
 
 # 2. separate product code and number
 df_data <- separate(df_data, Product.code...number., into=c("product_code", "product_number"), sep='-')
@@ -36,5 +44,5 @@ df_data <- df_data %>% unite(full_address, address, city, country, sep=", ")
 df_data <- df_data %>% mutate(product_category = paste("product_", tolower(product_category), sep="")) %>% spread(product_category, truth, is.na(0)) 
 
 # 6. write to CSV file
-View(df_data)
+#View(df_data)
 # write.table(df_data, file="data/refine_clean.csv",sep=',', append=F, row.names=F)
